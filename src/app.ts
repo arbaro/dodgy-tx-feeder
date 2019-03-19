d7677fc630eb76de1fcf1d088521c7ff3cca8a6c
John Williamson <johnjwilliamson@live.com>
2019-03-19 20:31:51 +1000

    a

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,9 @@ import { goDemux } from "./goDemux";
 import { goDfuse } from "./goDfuse";
 import * as mongoose from "mongoose";
 
+const fetch = require("node-fetch");
+import { JsonRpc } from "eosjs";
+
 dotenv.config();
 
 const { NODE_ENV, PRODUCTION_CONTRACT, DEVELOPMENT_CONTRACT } = process.env;
@@ -28,6 +31,32 @@ class ClaimTime extends Typegoose {
 
   @prop()
   role: string;
+
+  @prop()
+  reward: {
+    amount: number;
+    symbol: string;
+  };
+
+  @prop()
+  blockTime: Date;
+}
+
+class Org extends Typegoose {
+  @prop()
+  owner: string;
+
+  @prop()
+  tokensym: string;
+
+  @prop()
+  tokencon: string;
+
+  @prop()
+  friendlyname: string;
+
+  @prop()
+  blockTime: string;
 }
 
 class TokenTransfer extends Typegoose {
@@ -52,27 +81,59 @@ const main = async () => {
   );
   await mongoose.connection.dropDatabase();
 
+  const rpc = new JsonRpc("http://localhost:8888", { fetch });
+
   const ClaimTimeModel = new ClaimTime().getModelForClass(ClaimTime);
-  // const TransferModel = new TokenTransfer().getModelForClass(TokenTransfer);
+  const OrgModel = new Org().getModelForClass(Org);
+
   const handlers: Handler[] = [
     {
       versionName: "v1",
       actionType: `${contractName}::claimtime`,
       apply: async (payload: any) => {
         try {
-          console.log(payload);
+          const result = await rpc.history_get_transaction(
+            payload.transactionId
+          );
+          const [
+            amount,
+            symbol
+          ] = result.traces[0].inline_traces[0].act.data.quantity.split(" ");
+          const blockTime = result.block_time;
+          const reward = { amount, symbol };
           await ClaimTimeModel.create({
             ...payload.data,
             transactionId: payload.transactionId,
-            worker: payload.authorization[0].actor
+            worker: payload.authorization[0].actor,
+            reward,
+            blockTime
           });
           console.log("Commited:", payload.data.notes);
         } catch (e) {
           console.warn(
             `Failed commiting action ${payload.data.worker} of ${
               payload.data.dechours
-            } hours to database`
+            } hours to database ${e}`
           );
+        }
+      }
+    },
+    {
+      versionName: "v1",
+      actionType: `${contractName}::createorg`,
+      apply: async (payload: any) => {
+        try {
+          const result = await rpc.history_get_transaction(
+            payload.transactionId
+          );
+          console.log(result.block_time);
+          await OrgModel.create({
+            ...payload.data,
+            blockTime: result.block_time
+          });
+          console.log(payload);
+        } catch (e) {
+          console.warn(e);
         }
       }
     }
